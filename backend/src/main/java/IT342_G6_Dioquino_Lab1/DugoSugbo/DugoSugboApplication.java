0294f4fc6fe7ef6006deffdb1d9b409082189181
John Lawrence Dioquino <159016024+Penguinqt@users.noreply.github.com>
2026-04-07 20:35:59 +0800

    first commit

---
+++ backend/src/main/java/IT342_G6_Dioquino_Lab1/DugoSugbo/DugoSugboApplication.java
@@ -0,0 +1,13 @@
+package IT342_G6_Dioquino_Lab1.DugoSugbo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DugoSugboApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DugoSugboApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/IT342_G6_Dioquino_Lab1/DugoSugbo/DugoSugboApplicationTests.java
@@ -0,0 +1,13 @@
+package IT342_G6_Dioquino_Lab1.DugoSugbo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DugoSugboApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
